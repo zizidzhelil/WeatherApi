073ff9697dfc341b8de594e6e6beea1706ebadd8
Zeyneb Dzhelil <zizi_h@outlook.com>
2018-12-16 23:12:04 +0200

    Add missing tests.

---
+++ Source/Core.Tests/Models/CoordTests/LatitudePropertyTests.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using NUnit.Framework;
+
+namespace Core.Tests.Models.CoordTests
+{
+   [TestFixture]
+   public class LatitudePropertyTests
+   {
+      [Test]
+      public void GetAndSetLatitude()
+      {
+         double expected = 2.123123;
+
+         Coord coord = new Coord();
+         coord.Latitude = expected;
+
+         double actual = coord.Latitude;
+
+         Assert.AreEqual(actual, expected);
+      }
+   }
+}
